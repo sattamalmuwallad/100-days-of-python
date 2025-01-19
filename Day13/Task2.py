58ffeb0af08c77dde9f0e0b3bc91ceba1d984809
Sattam Almuwallad <sattam@sattams-MacBook-Pro.local>
2025-01-19 21:59:06 +0000

    Update

--- Day13/Task2.py
+++ Day13/Task2.py
@@ -1,4 +1,4 @@
 from random import randint
 dice_images = ["❶", "❷", "❸", "❹", "❺", "❻"]
-dice_num = randint(0, 5) # fixed so the range is in the list of images
+dice_num = randint(0, 5) # fixed so the range is in the list of image
 print(dice_images[dice_num])
