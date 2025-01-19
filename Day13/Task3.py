db2df54cfbe6fd8bcc506fc96efadeb2ee18a9ad
sattamalmuwallad <sattamalmuwallad@gmail.com>
2025-01-19 22:02:51 +0000

    update

--- Day13/Task3.py
+++ Day13/Task3.py
@@ -2,5 +2,5 @@ year = int(input("What's your year of birth?"))
 
 if year > 1980 and year < 1994:
     print("You are a millennial.")
-elif year >= 1994: # fixed so when adding 1994 no bugs
+elif year >= 1994: # fixed so when adding 1994 year no bugs
     print("You are a Gen Z.")
