4b90a7acffc882385035426fd4f696c5aa216b1f
sattamalmuwallad <sattamalmuwallad@gmail.com>
2025-02-07 18:45:35 +0000

    Day 16

--- Day 16/Task1.py
+++ Day 16/Task1.py
@@ -1,15 +1,6 @@
 from prettytable import PrettyTable
 
-# Create a PrettyTable object
 table = PrettyTable()
-
-# Define column names
-table.field_names = ["Name", "Age", "Country"]
-
-# Add some rows
-table.add_row(["Sattam", 21, "Saudi Arabia"])
-table.add_row(["Alice", 25, "UK"])
-table.add_row(["Bob", 23, "USA"])
-
-# Print the table
+table.add_column("Pokemon name",["Pikachu","Squirtle","Charmander"] )
+table.add_column("Type",["Electric", "Water", "Fire"])
 print(table)
