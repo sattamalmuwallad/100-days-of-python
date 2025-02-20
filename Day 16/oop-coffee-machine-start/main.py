6f1c9b3eb27baeaea5c01c312ec65f9f818fb6c1
Sattam Almuwallad <sattamalmuwallad@gmail.com>
2025-02-20 14:44:19 +0000

    Day 16

--- Day 16/oop-coffee-machine-start/main.py
+++ Day 16/oop-coffee-machine-start/main.py
@@ -3,7 +3,10 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 moneymachine = MoneyMachine()
-moneymachine.report()
 coffeemaker = CoffeeMaker()
-coffeemaker.report()
+menu = Menu()
 
+order = True
+
+print(menu.get_items())
+## choice = input(print(f"What would you like? {menu.get_items()}"))
